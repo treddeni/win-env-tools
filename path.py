66b5f3eea70ae2759f424f701d916878931be47d
Phill <ptreddenick@gmail.com>
2015-06-16 18:42:37 -0500

    Added screenshot of PathEditor

--- path.py
+++ path.py
@@ -1,15 +1,17 @@
 import winreg
 
+PARENT_KEY = "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment"
+
 def getPath():
-	key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment")
+	key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, PARENT_KEY)
 	return winreg.QueryValueEx(key, "Path")[0];
 	
 def setPath(newPath):
-	key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", 0, winreg.KEY_ALL_ACCESS)
+	key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, PARENT_KEY, 0, winreg.KEY_ALL_ACCESS)
 	winreg.SetValueEx(key, "Path", 0, winreg.REG_EXPAND_SZ, newPath)
 	
 def appendToPath(appendPath):
-	key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", 0, winreg.KEY_ALL_ACCESS)	
+	key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, PARENT_KEY, 0, winreg.KEY_ALL_ACCESS)	
 	path = winreg.QueryValueEx(key, "Path")[0];
 	if path[len(path)-1] != ';':
 		path += ';'
